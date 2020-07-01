74c7964445252d6aa2b1c4704b6e4552edfb1274
zs <zs@192.168.12.106>
2020-07-01 18:07:09 +0800

    使用Sentinel 是面向微服务的轻量级流量控制框架，从流量控制、熔断降级、系统负载保护等多个维度保护服务的稳定性。

---
+++ zhongshao/microservice-alibaba-sentinel/src/main/java/org/microservice/alibaba/sentinel/controller/InitController.java
@@ -0,0 +1,20 @@
+package org.microservice.alibaba.sentinel.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author Wangfan
+ */
+@RestController
+public class InitController {
+
+	//http://localhost:9092/hello
+	@GetMapping("/hello")
+	public String hello() {
+		System.out.println("microservice-alibaba-sentinel");
+		return "Hello, greetings from microservice-alibaba-sentinel";
+	}
+}
