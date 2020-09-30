4b55b8691db49a5276004be8730ad541025e729a
zs <zs@zs-PC>
2020-09-30 11:10:31 +0800

    springboot项目对数据库链接配置  加密操作

---
+++ zhongshao/microservice-alibaba-nacos-discovery/src/main/java/ink/zhongshao/TestEncrept.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package ink.zhongshao;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
+/**
+ * @author zs
+ * @date 2020年9月30日
+ */
+public class TestEncrept {
+
+	 public static void main(String[] arg){
+		 
+	        StandardPBEStringEncryptor standardPBEStringEncryptor =new StandardPBEStringEncryptor();
+	        /*配置文件中配置如下的算法*/
+	        standardPBEStringEncryptor.setAlgorithm("PBEWithMD5AndDES");
+	        /*配置文件中配置的password*/
+	        standardPBEStringEncryptor.setPassword("EWRREWRERWECCCXC");
+	        /*要加密的文本*/
+	        String name = standardPBEStringEncryptor.encrypt("root");
+	        String password =standardPBEStringEncryptor.encrypt("root");
+	        /*将加密的文本写到配置文件中*/
+	        System.out.println("name="+name);
+	        System.out.println("password="+password);
+	        
+	        
+	        //name=2RlCNEBxi+G3jzVvQ01HnQ==
+	        //password=eIQtVixBMIq+tdNy8+sRhA==
+	    }
+
+}
--- zhongshao/microservice-alibaba-nacos-discovery/src/main/java/ink/zhongshao/controller/InitController.java
+++ zhongshao/microservice-alibaba-nacos-discovery/src/main/java/ink/zhongshao/controller/InitController.java
@@ -1,6 +1,13 @@
 package ink.zhongshao.controller;
 
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class InitController {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	/**
 	 * http://localhost:9090/hello
@@ -19,6 +29,16 @@ public class InitController {
 	@GetMapping("/hello")
 	public String hello() {
 		System.out.println("microservice-alibaba-nacos-discovery");
+
+		Map<String, Object> result = this.jdbcTemplate.queryForMap("select * from user u limit 1");
+
+		if (!CollectionUtils.isEmpty(result)) {
+			Set<String> sets = result.keySet();
+			sets.forEach((key) -> {
+				System.out.println(key + ":" + result.get(key));
+			});
+		}
+
 		return "Hello, greetings from microservice-alibaba-nacos-discovery";
 	}
 
