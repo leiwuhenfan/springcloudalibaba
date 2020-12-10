ce6c193375592c571835ed48eb45740c67afaf53
zs <zs@zs-PC>
2020-12-10 17:54:15 +0800

    新增网关geteway项目

---
+++ zhongshao/microservice-alibaba-geteway/src/main/java/ink/zhongshao/Application.java
@@ -0,0 +1,23 @@
+package ink.zhongshao;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+/**
+ * 网关服务
+ * 
+ * @author Wangfan
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ zhongshao/microservice-alibaba-geteway/src/main/java/ink/zhongshao/utils/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 服务工具包
+ */
+/**
+ * @author zs
+ * @date 2020年12月9日
+ */
+package ink.zhongshao.utils;
