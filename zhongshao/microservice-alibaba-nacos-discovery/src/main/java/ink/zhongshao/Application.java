09d52dbbf22ebef3e7aeeb3502638d8de254cad5
454700515@qq.com <zs@zs-PC>
2021-07-14 10:04:50 +0800

    数据库连接池

--- zhongshao/microservice-alibaba-nacos-discovery/src/main/java/ink/zhongshao/Application.java
+++ zhongshao/microservice-alibaba-nacos-discovery/src/main/java/ink/zhongshao/Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 
 /**
  * 
@@ -19,5 +22,16 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	void createMysql1DatasourcePool(){
+    	HikariConfig config = new HikariConfig();
+    	config.setJdbcUrl("jdbc:mysql://localhost:3306/simpsons");
+    	config.setUsername("bart");
+    	config.setPassword("51mp50n");
+    	config.addDataSourceProperty("cachePrepStmts", "true");
+    	config.addDataSourceProperty("prepStmtCacheSize", "250");
+    	config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
 
+    	HikariDataSource ds = new HikariDataSource(config);
+    }
 }
